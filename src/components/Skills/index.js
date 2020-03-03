53ed91e822047a0669e078b672335f0f61e41f65
Guerra <lefipe.guerra@gmail.com>
2020-03-03 01:47:46 +0000

    [feature] json component for skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,85 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './styles.scss';
 
 import Code from '../../images/code-solid.svg';
 import Check from '../../images/check-circle-solid.svg';
 
-const Skills = () => {
-  return (
-    <section id="skills">
-      <div className="container">
-        <div className="flex-column">
-          <h2>Skills</h2>
-          <div className="subtitle-line">
-            <span><Code/></span>
-          </div>
-          <p className="text-large">
-            Knowledge acquired and evolving every day!
-          </p>
-        </div>
 
-        <div className="flex-row">
-          <div className="skills-col">
+class SkillList extends React.Component {
+  render() {
+    const skillData = require('../../services/skills.json');
+    return (
+      Object.keys(skillData).map(function(key) {
+        return (
+          <div className="skills-col" key={key}>
             <section>
-              <h3>Front-end</h3>
+              <h3>{key}</h3>
               <ul className="item-skills">
-                <li>HTML<Check /></li>
-                <li>CSS<Check /></li>
-                <li>Javascript<Check /></li>
-                <li>ReactJS<Check /></li>
-                <li>jQuery<Check /></li>
-                <li>Bootstrap<Check /></li>
-                <li>Sass<Check /></li>
-                <li>Webpack<Check /></li>
-                <li>Gulp<Check /></li>
-                <li>Responsive layout<Check /></li>
-                <li>Cross-Browser<Check /></li>
+                {skillData[key].map(function(value, key) {
+                  return (
+                    <li key={key}>{value}</li>
+                  );
+                })}
               </ul>
             </section>
           </div>
-
-          <div className="skills-col">
-            <section>
-              <h3>Platforms/CMS</h3>
-              <ul className="item-skills">
-                <li>Vtex Cloud<Check /></li>
-                <li>Presta Shop<Check /></li>
-                <li>EZ Commerce<Check /></li>
-                <li>B-Seller<Check /></li>
-                <li>Tray<Check /></li>
-                <li>Wordpress<Check /></li>
-              </ul>
-            </section>
-          </div>
-
-          <div className="skills-col">
-            <section>
-              <h3>Back-end</h3>
-              <ul className="item-skills">
-                <li>Python<Check /></li>
-                <li>PHP<Check /></li>          
-                <li>MongoDB<Check /></li>
-                <li>MySQL<Check /></li>
-                <li>SQLServer<Check /></li>
-                <li>SQLite<Check /></li>
-              </ul>
-            </section>
-          </div>
-
-          <div className="skills-col">
-            <section>
-              <h3>Version Control</h3>
-              <ul className="item-skills">
-                <li>GIT<Check /></li>
-                <li>SVN<Check /></li>
-              </ul>
-            </section>
-          </div>                    
-        </div>
-      </div>
-    </section>
-  )
+        );
+      })
+    );
+  }
 }
 
-export default Skills;
+export default class Skills extends Component {
+  /* state = {
+    skills: []
+  }
+
+  async componentDidMount() {
+    this.loadSkills();
+  }
+
+  loadSkills = () => {    
+    const skillData = require('../../services/skills.json');
+    this.setState({skills: skillData})
+  } */
+
+  render() {
+    // const {skills} = this.state;
+    const skillData = require('../../services/skills.json');
+    return (      
+      <section id="skills">
+        {/* {console.log(skillData)} */}
+
+        <div className="container">
+          <div className="flex-column">
+            <h2>Skills</h2>
+            <div className="subtitle-line">
+              <span><Code/></span>
+            </div>
+            <p className="text-large">
+              Knowledge acquired and evolving every day!
+            </p>
+          </div>
+
+          <div className="flex-row">
+            <SkillList />                   
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundler.js'
